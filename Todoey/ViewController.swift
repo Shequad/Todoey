7afb5f29d47233dd6935067238e958f0ee4082f7
Richard <richard@profeciait.co.za>
2019-06-24 14:03:04 +0200

    todo list complete with datasource and delegate methods

---
+++ Todoey/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Todoey
+//
+//  Created by Richard Maleho on 2019/06/20.
+//  Copyright © 2019 Profecia IT. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
